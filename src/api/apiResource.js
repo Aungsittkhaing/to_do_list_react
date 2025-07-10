e728937546931829ad7d15aa99b9851d5496eabc
Aungsittkhaing <aungsittkhaing695@gmail.com>
2025-07-10 13:00:00 +0630

    api changing process two

--- src/api/apiResource.js
+++ src/api/apiResource.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:
-    "postgresql://neondb_owner:npg_YcEK90tnekTG@ep-divine-queen-aehb320l-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require",
+  baseURL: "http://localhost:3000",
 });
